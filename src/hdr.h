25d576828dba41aa26616fc10cc8c0a79af623dd
jptr218 <84092766+jptr218@users.noreply.github.com>
2021-08-03 11:46:22 +0100

    Create hdr.h

---
+++ src/hdr.h
@@ -0,0 +1,27 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <vector>
+#include <thread>
+
+#pragma comment (lib, "Ws2_32.lib")
+using namespace std;
+
+#pragma pack(1)
+struct http2_stream {
+    uint16_t zero = 0;
+    uint8_t len;
+    uint8_t type;
+    uint8_t flags;
+    uint32_t st_id;
+};
+
+SOCKET connect_host(string ip, string port);
+void http2_initial(SOCKET sock);
+void worker(SOCKET sock, char* buf, int blen);
